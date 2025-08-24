a78b8cc8d0cfa43182576060b8155601950f3c4f
Mario Lopez <25378324+mardotio@users.noreply.github.com>
2025-08-24 14:37:16 -0400

    Add user and list details to whoami endpoint

--- services/api/internal/routes/users/whoami.go
+++ services/api/internal/routes/users/whoami.go
@@ -1,24 +1,60 @@
 package users
 
 import (
-	"github.com/go-chi/render"
-	"log"
 	"net/http"
+	"voteflix/api/internal/models"
 	"voteflix/api/internal/utils"
 )
 
 type whoAmIResponse struct {
-	UserId string `json:"userId"`
+	Id          string      `json:"id"`
+	DisplayName string      `json:"displayName"`
+	AvatarUrl   *string     `json:"avatarUrl"`
+	List        listDetails `json:"list"`
+}
+
+type listDetails struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	ServerId string `json:"serverId"`
 }
 
 func (rd whoAmIResponse) Render(w http.ResponseWriter, r *http.Request) error { return nil }
 
 func (h *Handler) WhoAmI(w http.ResponseWriter, r *http.Request) {
-	userClaims := utils.GetUserClaimsFromCtx(r.Context())
-	render.Status(r, http.StatusOK)
-	err := render.Render(w, r, whoAmIResponse{UserId: userClaims.Sub})
+	db := h.app.Db()
+	ctx := r.Context()
+	jsonSender := utils.NewJsonSender(w, r)
 
-	if err != nil {
-		log.Println(err)
+	userClaims := utils.GetUserClaimsFromCtx(ctx)
+
+	user := models.ListUser{}
+
+	userQuery := db.NewSelect().
+		Model(&user).
+		Where("list_user.id = ?", userClaims.Sub).
+		Relation("User").
+		Relation("List")
+
+	if err := userQuery.Scan(ctx); err != nil {
+		jsonSender.InternalServerError(err)
+		return
 	}
+
+	response := whoAmIResponse{
+		Id: user.Id,
+		List: listDetails{
+			Id:       user.List.Id,
+			Name:     user.List.Name,
+			ServerId: user.List.DiscordServerId,
+		},
+		AvatarUrl:   utils.GetAvatarUrl(*user.User),
+		DisplayName: user.User.DiscordUsername,
+	}
+
+	if user.DiscordNickname != nil {
+		response.DisplayName = *user.DiscordNickname
+	}
+
+	jsonSender.Ok(response)
 }
