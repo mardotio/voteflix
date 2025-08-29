cd2406f59349b0e79e2065516857704a4a62ea22
Mario Lopez <25378324+mardotio@users.noreply.github.com>
2025-08-29 14:59:52 -0400

    Add endpoint for creating movies

--- services/api/internal/models/movie.go
+++ services/api/internal/models/movie.go
@@ -6,10 +6,11 @@ type Movie struct {
 	bun.BaseModel `bun:"table:movies"`
 	PrimaryUuidId
 	Timestamps
-	ListId    string `bun:"list_id,type:uuid,notnull"`
-	Name      string `bun:"name,type:varchar(255),notnull"`
+	ListId string `bun:"list_id,type:uuid,notnull"`
+	Name   string `bun:"name,type:varchar(255),notnull"`
+	//pending,approved,watched,rejected
 	Status    string `bun:"status,type:varchar(15),notnull"`
-	Seed      int64  `bun:"seed,type:integer,notnull"`
+	Seed      int64  `bun:"seed,type:integer,default:gen_random_movie_seed()"`
 	CreatorId string `bun:"creator_id,type:uuid,notnull"`
 	Creator   *User  `bun:"rel:belongs-to,join:creator_id=id"`
 }
