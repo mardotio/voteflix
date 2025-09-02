bd606d107c22c97bf15430102868f6d33c71fce6
Mario Lopez <25378324+mardotio@users.noreply.github.com>
2025-09-02 16:47:33 -0400

    Add endpoint for voting on movie (#13)
    - Added approve/reject count columns on movie for optimistic locking
    - When movie reached approve/reject threshold it is moved to next
      status

--- services/api/internal/models/movie.go
+++ services/api/internal/models/movie.go
@@ -6,11 +6,12 @@ type Movie struct {
 	bun.BaseModel `bun:"table:movies"`
 	PrimaryUuidId
 	Timestamps
-	ListId string `bun:"list_id,type:uuid,notnull"`
-	Name   string `bun:"name,type:varchar(255),notnull"`
-	//pending,approved,watched,rejected
-	Status    string `bun:"status,type:varchar(15),notnull"`
-	Seed      int64  `bun:"seed,type:integer,default:gen_random_movie_seed()"`
-	CreatorId string `bun:"creator_id,type:uuid,notnull"`
-	Creator   *User  `bun:"rel:belongs-to,join:creator_id=id"`
+	ListId       string `bun:"list_id,type:uuid,notnull"`
+	Name         string `bun:"name,type:varchar(255),notnull"`
+	Status       string `bun:"status,type:varchar(15),notnull"` //pending, accepted, rejected, watched
+	Seed         int64  `bun:"seed,type:integer,default:gen_random_movie_seed()"`
+	ApproveCount int64  `bun:"approve_count,type:integer,default:0"`
+	RejectCount  int64  `bun:"reject_count,type:integer,default:0"`
+	CreatorId    string `bun:"creator_id,type:uuid,notnull"`
+	Creator      *User  `bun:"rel:belongs-to,join:creator_id=id"`
 }
--- services/api/internal/models/vote.go
+++ services/api/internal/models/vote.go
@@ -3,10 +3,10 @@ package models
 import "github.com/uptrace/bun"
 
 type Vote struct {
-	bun.BaseModel `bun:"table:vote"`
+	bun.BaseModel `bun:"table:votes"`
 	Timestamps
 	MovieId    string `bun:"movie_id,pk,type:uuid,notnull"`
-	UserId     string `bun:"list_user_id,pk,type:uuid,notnull"`
+	UserId     string `bun:"user_id,pk,type:uuid,notnull"`
 	IsApproval bool   `bun:"is_approval,type:boolean,notnull"`
 	Movie      *Movie `bun:"rel:belongs-to,join:movie_id=id"`
 	User       *User  `bun:"rel:belongs-to,join:user_id=id"`
